713ddb27761021633db171e58ba11d816458cbfc
David Ross <rustyross101@gmail.com>
2016-05-08 23:33:28 -0700

    Added garlic bread and insurance input methods

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,4 +12,26 @@ present_age = birth_year + age
   else
     present_age = false
   end
-  
+  present_year = 2015 || 2016
+
+puts "Would you like some garlic bread?"
+garlic_bread = gets.chomp
+  until garlic_bread == "yes" || garlic_bread == "no"
+  	puts "Your answer must be yes or no."
+  end
+    if garlic_bread == "yes"
+  			garlic_bread = true
+  	else
+  			garlic_bread= false
+  	end
+
+puts "Would you like to enroll in health insurance?"
+    insurance = gets.chomp
+    	until insurance == "yes" || insurance == "no"
+    		puts "Your answer must be yes or no."
+    	end
+    		if insurance == "yes"
+    			insurance = true
+    		else
+    			insurance = false
+    		end
