34bc19fc1fdee5d176f24c3fe03ee1e703ec5aff
David Ross <rustyross101@gmail.com>
2016-05-14 20:36:58 -0700

    Started detective work

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,13 +4,13 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# "zom".insert(2, 'o')
 # => “zoom”
 
-# "enhance".<???>
+# "enhance".
 # => "    enhance    "
 
 # "Stop! You’re under arrest!".<???>
@@ -32,7 +32,7 @@
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
 # "How many times does the letter 'a' appear in this string?".<???>
