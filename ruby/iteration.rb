b9f3a7d14477985279657210d924fff442d95f54
David Ross <rustyross101@gmail.com>
2016-05-14 18:14:55 -0700

    Added super hero power array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,21 @@ def cats
 end
 
 cats { |cat1, cat2| puts "My favorite cats are #{cat1} and #{cat2}" }
+
+super_heros = ["superman", "wonderwoman", "green latern", "flash"]
+
+powers = { superman: "flight", wonder_woman: "super strength", green_lantern: "conjuring objects out of light", flash: "time travel" }
+
+puts "Super Heros:"
+p super_heros
+
+super_heros.each do |name|
+	p name	
+end
+
+puts "Super hero powers:"
+p powers
+
+powers.each do |name, power|
+	puts " #{name} has the power of #{power}"
+end
