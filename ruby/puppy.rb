84cee63717cb636e0f111fdd2f368cfb097ddd77
David Ross <rustyross101@gmail.com>
2016-05-20 20:16:42 -0700

    Establishes puppy class psuedocode

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+#Species__________________
+	#Golden Retriever 
+#Characteristics__________
+	#Color: Gold
+	#Size: Large
+	#Hair legnth: Long
+	#Personality: Lovable
+	#Number of Legs: 4
+#Behavior_________________
+	#Run
+	#fetch
+	#Bark
+	#Lick
