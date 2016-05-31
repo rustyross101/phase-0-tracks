5b9dffc24ba616956a8c2ea117afeb2ac9a548c2
David Ross <rustyross101@gmail.com>
2016-05-31 08:28:19 -0700

    starts function and establishes array

---
+++ js/algos.js
@@ -0,0 +1,22 @@
+//establish function for finding longest phrase in an array
+//create an empty array
+//plug input into array
+//while i is less than array length loop though array
+//if array strings length is greater than legnth of previous string
+//then
+
+function longest_phrase;
+  var words = ["shortest", "second shortest", "definately that big mofo"];
+
+  var longest = 0;
+
+  for(var i = 0; i < words.length; i++) {
+    if(longest
+      var counter = arr[i].length;
+      longest = arr[i];
+    }
+  }
+
+//driver code
+
+return longest_phrase;
